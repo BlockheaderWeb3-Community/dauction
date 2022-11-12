4b286141a2140db7948f138ec9cf5049738e7362
sprtd <sagesdn@gmail.com>
2022-11-12 17:42:28 +0100

    test: revert reveal bid with insufficient bid token balance

--- test/DauctionTest.ts
+++ test/DauctionTest.ts
@@ -416,7 +416,7 @@ describe('Dauction Marketplace', async () => {
         to.be.revertedWith("auction not ended yet");
     })
 
-     it.only("should revert bidder attempt to reveal bid after reveal bid time", async () => {
+     it("should revert bidder attempt to reveal bid after reveal bid time", async () => {
       const addr2Salt = 5000
 
       const addr2BidValue = 5
@@ -430,6 +430,22 @@ describe('Dauction Marketplace', async () => {
       await mockWETH.connect(addr2).approve(dauction.address, parseEther('1000'))
       await expect(dauction.connect(addr2).revealBid(nftContract.address, 1, addr2BidValue, createSalt(addr2Salt))).
         to.be.revertedWith("not in reveal phase");
+    })
+
+     it.only("should revert bidder attempt to reveal bid with no or insufficient bidToken balance", async () => {
+      const addr2Salt = 5000
+
+      const addr2BidValue = 5
+      const AUCTION_PARAMS = [nftContract.address, 1, 5, setTime(1), setTime(2), setTime(4)] as const;
+      await nftContract.connect(addr1).approve(dauction.address, 1);
+      await dauction.connect(addr1).createAuction(...AUCTION_PARAMS);
+      increaseBlockTimestamp(1);
+      await dauction.connect(addr4).createBid(nftContract.address, 1, hashCommitmentParams(addr2BidValue, createSalt(addr2Salt)), mockWETH.address);
+      increaseBlockTimestamp(2)
+
+      // await mockWETH.connect(addr2).approve(dauction.address, parseEther('1'))
+      await expect(dauction.connect(addr4).revealBid(nftContract.address, 1, addr2BidValue, createSalt(addr2Salt))).
+        to.be.revertedWith("insuff token bal");
     })
   })
 })
